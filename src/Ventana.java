42a0106a6289832e91d12e65a95b4165fda21e64
JoseLuisGL <89991775+JoseLuisGL@users.noreply.github.com>
2023-03-24 20:25:07 -0700

    Se crean funciones de cuenta con su menu y acciones asi como tambien la funcion ayuda que solo contiene informacion. Se hacen mas validaciones en el registro y login

--- src/Ventana.java
+++ src/Ventana.java
@@ -10,11 +10,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Scanner;
 
 import javax.swing.ButtonGroup;
 import javax.swing.Icon;
@@ -39,9 +42,12 @@ import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 public class Ventana extends JFrame {
+	private static final ActionListener ActionListener = null;
 	private int duracion;
 	private String anterior = "login";
 	private String actual = "login";
+	private String cuenta = "login";
+	private String ayuda = "login";
 	public JPanel panel = null;
 	
 	public Ventana() {
@@ -98,11 +104,6 @@ public class Ventana extends JFrame {
 		jp1.setLayout(null);
 		jp1.setBackground(Color.decode("#FFB4B5"));
 		
-		JPanel jp3 = new JPanel();
-		jp3.setSize(500, 600);
-		jp3.setLocation(0, 0);
-		jp3.setLayout(null);
-		jp3.setBackground(Color.GRAY);
 		
 		//titulo
 		JLabel title = new JLabel("Accede a tu cuenta",JLabel.CENTER);
@@ -164,7 +165,7 @@ public class Ventana extends JFrame {
 				anterior = actual;
 				actual = "registro";
 				
-				add(jp3);
+				
 				String email = username.getText();
 				String pwd = new String (password.getPassword());
 				
@@ -196,59 +197,9 @@ public class Ventana extends JFrame {
 					if(flag) {
 						JOptionPane.showMessageDialog(null,"Bienvenido " + username.getText(),
 						"Ingresando al sistema...", JOptionPane.INFORMATION_MESSAGE );
-						JLabel bienvenida = new JLabel("<html>Bienvenido has hecho sesion como:<p><html>" + username.getText(),JLabel.CENTER);
-						bienvenida.setFont(new Font("Comic Sans", Font.BOLD,18));
-						bienvenida.setSize(350, 160);
-						bienvenida.setLocation(80, 50);
-						bienvenida.setOpaque(true);
-						bienvenida.setBackground(Color.GRAY);
-						
-						JButton btnVolver = new JButton("Cerrar sesion");
-						btnVolver.setSize(250,40);
-						btnVolver.setLocation(120,205);
-						jp3.add(btnVolver);
-						//Barra
-						JMenuBar barra = new JMenuBar();
-						jp3.add(barra);
-						JMenu Cuenta = new JMenu("Cuenta");
-						barra.add(Cuenta);
-						JMenu Usuarios = new JMenu("Usuarios");
-						barra.add(Usuarios);
-						JMenu Ayuda = new JMenu("Ayuda");
-						barra.add(Ayuda);
-						setJMenuBar(barra);
-						//Items y Acciones
-						JMenuItem i1 = new JMenuItem("Mi cuenta");
-						i1.addActionListener(new ActionListener() {
-
-							@Override
-							public void actionPerformed(ActionEvent e) {
-								// TODO Auto-generated method stub
-								remove(jp1);
-								remove(jp3);
-								add(jp1);
-								repaint();
-								revalidate();
-							}
-							
-						});
-						Cuenta.add(i1);
-						JMenuItem i2 = new JMenuItem("Cerrar sesion");
-						i2.addActionListener(new ActionListener() {
-
-							@Override
-							public void actionPerformed(ActionEvent e) {
-								// TODO Auto-generated method stub
-								anterior = actual;
-								actual = "login";
-								remove(jp3);
-								remove(barra);
-								limpiarVentana();
-							}
-			
-						});
-						Cuenta.add(i2);
-						jp3.add(bienvenida);
+						anterior = actual;
+						actual = "cuenta";
+						add(cuenta());
 						repaint();
 						revalidate();
 					}else {
@@ -268,8 +219,111 @@ public class Ventana extends JFrame {
 				
 		}
 			
+		public JPanel cuenta() {
+			JLabel bienvenida = new JLabel("<html>Bienvenido has hecho sesion como:<p><html>" ,JLabel.CENTER);
+			bienvenida.setFont(new Font("Comic Sans", Font.BOLD,18));
+			bienvenida.setSize(350, 160);
+			bienvenida.setLocation(80, 20);
+			bienvenida.setOpaque(true);
+			bienvenida.setBackground(Color.GRAY);
+			
+			JPanel jp3 = new JPanel();
+			jp3.setSize(500, 600);
+			jp3.setLocation(0, 0);
+			jp3.setLayout(null);
+			jp3.setBackground(Color.GRAY);
+			
+			JLabel image1 = new JLabel();
+			image1.setSize(200, 200);
+			image1.setLocation(160, 150);
+			ImageIcon imagen = new ImageIcon("img/inicio.png");
+			Icon icono = new ImageIcon(imagen.getImage().getScaledInstance(image1.getWidth(), image1.getHeight(), Image.SCALE_DEFAULT));
+			image1.setIcon(icono);
+			jp3.add(image1);
+			
+			//Barra
+			JMenuBar barra = new JMenuBar();
+			jp3.add(barra);
+			JMenu Cuenta = new JMenu("Cuenta");
+			barra.add(Cuenta);
+			JMenu Usuarios = new JMenu("Usuarios");
+			barra.add(Usuarios);
+			JMenu Ayuda = new JMenu("Ayuda");
+			barra.add(Ayuda);
+			setJMenuBar(barra);
+			//Items y Acciones
+			JMenuItem i1 = new JMenuItem("Mi cuenta");
+			i1.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+			});
+			Cuenta.add(i1);
+			JMenuItem i2 = new JMenuItem("Cerrar sesion");
+			i2.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					anterior = actual;
+					actual = "login";
+					remove(jp3);
+					limpiarVentana();
+				}
+
+			});
+			Cuenta.add(i2);
+			JMenuItem i3 = new JMenuItem("Lista de usuarios");
+			i3.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+			});
+			Usuarios.add(i3);
+			JMenuItem i4 = new JMenuItem("Crear cuenta");
+			i4.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					remove(jp3);
+					remove(login());
+					anterior = actual;
+					actual = "registro";
+					
+					limpiarVentana();
+				}
+				
+			});
+			Usuarios.add(i4);
+			JMenuItem i5 = new JMenuItem("¿Como crear usuarios?");
+			i5.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					remove(jp3);
+					remove(login());
+					anterior = actual;
+					actual = "ayuda";
+					
+					limpiarVentana();
+				}
+				
+			});
+			Ayuda.add(i5);
+			jp3.add(bienvenida);
+			return jp3;
+		}
 		
-	
 		public JPanel registro() {
 
 		
@@ -280,82 +334,156 @@ public class Ventana extends JFrame {
 			jp2.setSize(500, 600);
 			jp2.setLocation(0, 0);
 			jp2.setLayout(null);
-			jp2.setBackground(Color.decode("#C45EDF"));
+			jp2.setBackground(Color.decode("#FFB4B5"));
+			
+			JLabel image1 = new JLabel();
+			image1.setSize(90, 90);
+			image1.setLocation(200, 30);
+			ImageIcon imagen = new ImageIcon("img/perfil.png");
+			Icon icono = new ImageIcon(imagen.getImage().getScaledInstance(image1.getWidth(), image1.getHeight(), Image.SCALE_DEFAULT));
+			image1.setIcon(icono);
+			jp2.add(image1);
 			
 			//titulo
-			JLabel titleR = new JLabel("Registro nuevo usuario",JLabel.CENTER);
-			titleR.setFont(new Font("Comic Sans", Font.BOLD,20));
-			titleR.setSize(280, 40);
-			titleR.setLocation(100, 20);
+			JLabel titleR = new JLabel("Crear Usuario",JLabel.CENTER);
+			titleR.setFont(new Font("Comic Sans", Font.BOLD,24));
+			titleR.setSize(280, 20);
+			titleR.setLocation(100, 10);
 			titleR.setOpaque(true);
-			titleR.setBackground(Color.green);
+			titleR.setBackground(Color.decode("#FFB4B5"));
 			jp2.add(titleR);
 			
-			JLabel tag1R = new JLabel("Ingrese el nombre de usuario: ",JLabel.CENTER);
+			JLabel tag1R = new JLabel("Nombre: ",JLabel.CENTER);
 			tag1R.setSize(250, 20);
-			tag1R.setLocation(120, 80);
+			tag1R.setLocation(120, 120);
 			tag1R.setOpaque(true);
-			tag1R.setBackground(Color.black);
+			tag1R.setBackground(Color.decode("#FFB4B5"));
 			tag1R.setForeground(Color.white);
 			jp2.add(tag1R);
 			
-			JTextField usernameR = new JTextField("");
-			usernameR.setSize(250, 40);
-			usernameR.setLocation(120, 100);
-			jp2.add(usernameR);
+			JTextField nombres = new JTextField("");
+			nombres.setSize(250, 40);
+			nombres.setLocation(120, 140);
+			jp2.add(nombres);
 			
-			JLabel tag2R = new JLabel("Ingrese el correo electrónico: ",JLabel.CENTER);
+			JLabel tag2R = new JLabel("Usuario: ",JLabel.CENTER);
 			tag2R.setSize(250, 20);
-			tag2R.setLocation(120, 140);
+			tag2R.setLocation(120, 180);
 			tag2R.setOpaque(true);
-			tag2R.setBackground(Color.black);
+			tag2R.setBackground(Color.decode("#FFB4B5"));
 			tag2R.setForeground(Color.white);
 			jp2.add(tag2R);
 			
-			JPasswordField passwordR = new JPasswordField();
-			passwordR.setSize(250, 40);
-			passwordR.setLocation(120, 160);
-			jp2.add(passwordR);
+			JTextField usuarios = new JTextField();
+			usuarios.setSize(250, 40);
+			usuarios.setLocation(120, 200);
+			jp2.add(usuarios);
 			
-			JLabel tag3 = new JLabel("Ingrese el nombre: ",JLabel.CENTER);
+			JLabel tag3 = new JLabel("Email: ",JLabel.CENTER);
 			tag3.setSize(250, 20);
-			tag3.setLocation(120, 210);
+			tag3.setLocation(120, 240);
 			tag3.setOpaque(true);
-			tag3.setBackground(Color.black);
+			tag3.setBackground(Color.decode("#FFB4B5"));
 			tag3.setForeground(Color.white);
 			jp2.add(tag3);
 			
-			JTextField name = new JTextField("");
-			name.setSize(250, 40);
-			name.setLocation(120, 250);
-			jp2.add(name);
+			JTextField correo = new JTextField();
+			correo.setSize(250, 40);
+			correo.setLocation(120, 260);
+			jp2.add(correo);
 			
-			JLabel tag4 = new JLabel("Ingrese el nombre: ",JLabel.CENTER);
+			JLabel tag4 = new JLabel("Contraseña: ",JLabel.CENTER);
 			tag4.setSize(250, 20);
 			tag4.setLocation(120, 300);
 			tag4.setOpaque(true);
-			tag4.setBackground(Color.black);
+			tag4.setBackground(Color.decode("#FFB4B5"));
 			tag4.setForeground(Color.white);
 			jp2.add(tag4);
 			
-			JTextField lastname = new JTextField("");
-			lastname.setSize(250, 40);
-			lastname.setLocation(120, 340);
-			jp2.add(lastname);
+			JPasswordField contraseña1 = new JPasswordField();
+			contraseña1.setSize(250, 40);
+			contraseña1.setLocation(120, 320);
+			jp2.add(contraseña1);
 			
-			JButton btnAdd = new JButton("Registrate!");
-			btnAdd.setSize(250,40);
-			btnAdd.setLocation(120, 390);
-			jp2.add(btnAdd);
+			JLabel tag5 = new JLabel("Confirmar contraseña: ",JLabel.CENTER);
+			tag5.setSize(250, 20);
+			tag5.setLocation(120, 360);
+			tag5.setOpaque(true);
+			tag5.setBackground(Color.decode("#FFB4B5"));
+			tag5.setForeground(Color.white);
+			jp2.add(tag5);
 			
-			btnAdd.addActionListener(new ActionListener() {
+			JPasswordField contraseña2 = new JPasswordField();
+			contraseña2.setSize(250, 40);
+			contraseña2.setLocation(120, 380);
+			jp2.add(contraseña2);
+			
+			JButton btnCrear = new JButton("Crear usuario");
+			btnCrear.setSize(120,40);
+			btnCrear.setLocation(250, 450);
+			jp2.add(btnCrear);
+			
+			btnCrear.addActionListener(new ActionListener() {
+			    @Override
+			    public void actionPerformed(ActionEvent e) {
+			        // Verificar que todos los campos estén llenos
+			        if (nombres.getText().isEmpty() || usuarios.getText().isEmpty() ||
+			                correo.getText().isEmpty() || contraseña1.getPassword().length == 0 || contraseña2.getPassword().length == 0) {
+			            JOptionPane.showMessageDialog(jp2, "Por favor, llene todos los campos.");
+			            return;
+			        }
+			        
+			        String nombre = nombres.getText();
+			        String usuario = usuarios.getText();
+			        String email = correo.getText();
+			        String pwd = new String(contraseña1.getPassword());
+			        
+			        // Verificar si ya existe un correo electrónico igual
+			        try {
+			            Scanner scanner = new Scanner(new File("users.txt"));
+			            while (scanner.hasNextLine()) {
+			                String line = scanner.nextLine();
+			                String[] parts = line.split(",");
+			                if (parts.length >= 3 && parts[2].equals(email)) {
+			                    JOptionPane.showMessageDialog(jp2, "No se pudo crear el usuario. Ya existe un correo electrónico igual.");
+			                    scanner.close();
+			                    return;
+			                }
+			            }
+			            scanner.close();
+			        } catch (FileNotFoundException ex) {
+			            ex.printStackTrace();
+			        }
+			        
+			        // Guardar el nuevo usuario en el archivo
+			        try {
+			            FileWriter writer = new FileWriter("users.txt", true);
+			            PrintWriter linea = new PrintWriter(writer);
+			            linea.println(nombre + "," + usuario + "," + email + "," + pwd);
+			            linea.close();
+			            writer.close();
+			            
+			            JOptionPane.showMessageDialog(jp2, "Usuario creado");
+			        } catch (IOException e1) {
+			            e1.printStackTrace();
+			            JOptionPane.showMessageDialog(jp2, "El usuario no se ha podido crear");
+			        }
+			    }
+			});
+			
+			JButton btnVolver = new JButton("Cancelar");
+			btnVolver.setSize(120,40);
+			btnVolver.setLocation(120, 450);
+			jp2.add(btnVolver);
+			
+			btnVolver.addActionListener(new ActionListener() {
 	
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					// TODO Auto-generated method stub
 					
 					anterior = actual;
-					actual = "login";
+					actual = "cuenta";
 					
 					limpiarVentana();
 					
@@ -369,31 +497,175 @@ public class Ventana extends JFrame {
 		
 		public void limpiarVentana() {
 		
-		if(panel!= null) {
-			this.remove(panel);
-		}
-		
-		if(actual.equals("login")){
-			panel = login(); 
+			if(panel!= null) {
+				this.remove(panel);
+			}
 			
-			this.add(panel);
+			if(actual.equals("login")){
+				panel = login(); 
+				
+				this.add(panel);
+				
+				this.repaint();
+				this.revalidate();
+			}
 			
-			this.repaint();
-			this.revalidate();
-		}
-		
-		if(actual.equals("registro")){
-			panel = registro();
-			
-			this.add(panel);
-			
-			this.repaint();
-			this.revalidate();
+			if(actual.equals("registro")){
+				panel = registro();
+				
+				this.add(panel);
+				
+				this.repaint();
+				this.revalidate();
+			}
+			if(actual.equals("cuenta")){
+				panel = cuenta();
+				
+				this.add(panel);
+				
+				this.repaint();
+				this.revalidate();
+			}
+			if(actual.equals("ayuda")){
+				panel = ayuda();
+				
+				this.add(panel);
+				
+				this.repaint();
+				this.revalidate();
+			}
+	
 		}
 	
-}
-	
-	
+		public JPanel ayuda() {
+			anterior = actual;
+			actual = "ayuda";
+			JPanel jp4 = new JPanel();
+			jp4.setSize(500, 600);
+			jp4.setLocation(0, 0);
+			jp4.setLayout(null);
+			jp4.setBackground(Color.decode("#FFB4B5"));
+			
+			JLabel ayudatitulo = new JLabel("¿Como crear un usuario?" ,JLabel.CENTER);
+			ayudatitulo.setFont(new Font("Comic Sans", Font.BOLD,24));
+			ayudatitulo.setSize(350, 30);
+			ayudatitulo.setLocation(80, 10);
+			ayudatitulo.setOpaque(true);
+			ayudatitulo.setForeground(Color.white);
+			ayudatitulo.setBackground(Color.decode("#FFB4B5"));
+			
+			JLabel image1 = new JLabel();
+			image1.setSize(120, 120);
+			image1.setLocation(190, 50);
+			ImageIcon imagen = new ImageIcon("img/ayuda.png");
+			Icon icono = new ImageIcon(imagen.getImage().getScaledInstance(image1.getWidth(), image1.getHeight(), Image.SCALE_DEFAULT));
+			image1.setIcon(icono);
+			jp4.add(image1);
+			
+			JPanel jpinstrucciones = new JPanel();
+			jpinstrucciones.setSize(460, 330);
+			jpinstrucciones.setLocation(10, 200);
+			jpinstrucciones.setLayout(null);
+			jpinstrucciones.setBackground(Color.decode("#FE8486"));
+			
+			JLabel instruccion1 = new JLabel("<html>1.- Hacer click en la opcion (Usuarios) en el<p> menu superior<html> " ,JLabel.LEFT);
+			instruccion1.setFont(new Font("Comic Sans", Font.BOLD,16));
+			instruccion1.setSize(500, 50);
+			instruccion1.setLocation(10, 10);
+			instruccion1.setOpaque(true);
+			instruccion1.setForeground(Color.white);
+			instruccion1.setBackground(Color.decode("#FE8486"));
+			
+			JLabel instruccion2 = new JLabel("<html>2.- Hacer click en la opcion (Crear Usuario) en el<p> menu desplegado<html> " ,JLabel.LEFT);
+			instruccion2.setFont(new Font("Comic Sans", Font.BOLD,16));
+			instruccion2.setSize(500, 50);
+			instruccion2.setLocation(10, 60);
+			instruccion2.setOpaque(true);
+			instruccion2.setForeground(Color.white);
+			instruccion2.setBackground(Color.decode("#FE8486"));
+			
+			JLabel instruccion3 = new JLabel("<html>3.- Llenar los campos solicitados<html> " ,JLabel.LEFT);
+			instruccion3.setFont(new Font("Comic Sans", Font.BOLD,16));
+			instruccion3.setSize(500, 50);
+			instruccion3.setLocation(10, 100);
+			instruccion3.setOpaque(true);
+			instruccion3.setForeground(Color.white);
+			instruccion3.setBackground(Color.decode("#FE8486"));
+			
+			JLabel instruccion4 = new JLabel("<html>4.- Escribe una pequeña descripcion de ti<html> " ,JLabel.LEFT);
+			instruccion4.setFont(new Font("Comic Sans", Font.BOLD,16));
+			instruccion4.setSize(500, 50);
+			instruccion4.setLocation(10, 130);
+			instruccion4.setOpaque(true);
+			instruccion4.setForeground(Color.white);
+			instruccion4.setBackground(Color.decode("#FE8486"));
+			
+			JLabel instruccion5 = new JLabel("<html>5.- Seleccionar tu comida favorita<html> " ,JLabel.LEFT);
+			instruccion5.setFont(new Font("Comic Sans", Font.BOLD,16));
+			instruccion5.setSize(500, 50);
+			instruccion5.setLocation(10, 160);
+			instruccion5.setOpaque(true);
+			instruccion5.setForeground(Color.white);
+			instruccion5.setBackground(Color.decode("#FE8486"));
+			
+			JLabel instruccion6 = new JLabel("<html>6.- Seleccionar tu color favorito<html> " ,JLabel.LEFT);
+			instruccion6.setFont(new Font("Comic Sans", Font.BOLD,16));
+			instruccion6.setSize(500, 50);
+			instruccion6.setLocation(10, 190);
+			instruccion6.setOpaque(true);
+			instruccion6.setForeground(Color.white);
+			instruccion6.setBackground(Color.decode("#FE8486"));
+			
+			JLabel instruccion7 = new JLabel("<html>7.-Hacer click en el boton (Crea Usuario)<html> " ,JLabel.LEFT);
+			instruccion7.setFont(new Font("Comic Sans", Font.BOLD,16));
+			instruccion7.setSize(500, 50);
+			instruccion7.setLocation(10, 220);
+			instruccion7.setOpaque(true);
+			instruccion7.setForeground(Color.white);
+			instruccion7.setBackground(Color.decode("#FE8486"));
+			
+			JLabel instruccion8 = new JLabel("<html>8.- Listo el usuario se ha creado<html> " ,JLabel.LEFT);
+			instruccion8.setFont(new Font("Comic Sans", Font.BOLD,16));
+			instruccion8.setSize(500, 50);
+			instruccion8.setLocation(10, 250);
+			instruccion8.setOpaque(true);
+			instruccion8.setForeground(Color.white);
+			instruccion8.setBackground(Color.decode("#FE8486"));
+			
+			JButton btn1 = new JButton("Crear un usuario ahora");
+			btn1.setBackground(Color.cyan);
+			btn1.setForeground(Color.white);
+			btn1.setSize(230,30);
+			btn1.setLocation(120,295);
+			btn1.addActionListener(new ActionListener(){
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					remove(jp4);
+					remove(login());
+					anterior = actual;
+					actual = "registro";
+					
+					limpiarVentana();
+				}
+				
+			});
+			
+			jp4.add(ayudatitulo);
+			jpinstrucciones.add(instruccion1);
+			jpinstrucciones.add(instruccion2);
+			jpinstrucciones.add(instruccion3);
+			jpinstrucciones.add(instruccion4);
+			jpinstrucciones.add(instruccion5);
+			jpinstrucciones.add(instruccion6);
+			jpinstrucciones.add(instruccion7);
+			jpinstrucciones.add(instruccion8);
+			jpinstrucciones.add(btn1);
+			
+			jp4.add(jpinstrucciones);
+			return jp4;
+		}
 	
 	
 	
